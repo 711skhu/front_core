1eff76a0d53fca411a37566d20879fbff9a1fd36
shouwn <sjonghyon@gmail.com>
2019-04-07 07:45:04 +0900

    #1 SelectableList 리팩토링 - props 추가: 라인 색상, 라인 굵기 - props 추가에 따른 메소드 추가 updateHoverState, isHighlighted, getLineStyle - SelectableList 샘플 작성

--- src/components/problem/SelectableList.vue
+++ src/components/problem/SelectableList.vue
@@ -3,15 +3,17 @@
     <div
       v-for="(item, index) in items"
       :key="index"
+      :style="getLineStyle(index)"
       class="itemContainer"
-      :class="[{ selected: index === local_selectedIndex }]"
       @click.stop="clickItem(item, index)"
+      @mouseenter="updateHoverState(index, true)"
+      @mouseleave="updateHoverState(index, false)"
     >
       <slot name="item" :element="item">
         <!-- 대체 컨텐츠 -->
       </slot>
     </div>
-    <div style="clear:both"></div>
+    <div style="clear:both"></div> <!-- float: left 로 인한 태그 및 스타일 -->
   </div>
 </template>
 
@@ -22,7 +24,14 @@
         type: Array,
         required: true
       },
-      selectedIndex: {
+      lineColor: { // under line 색
+        type: String,
+        required: true
+      },
+      lineSize: { //  under line 굵기
+        required: true
+      },
+      selectedIndex: { // 초기 선택 index - 초기에만 사용되고 변경 되어도 영향을 받지 않음
         type: Number,
         default: 0
       }
@@ -30,12 +39,32 @@
     data () {
       return {
         local_selectedIndex: this.selectedIndex, // 선택된 아이템의 index
+        hoverItemIndex: -1, // hover 된 아이템의 index
+        blankLine: { // 선택되지 않았을 때 보여질 투명 라인
+          borderBottom: this.lineSize + " solid #00000000"
+        },
+        underLine: { // 선택되었을 때 보여질 색이 있는 라인
+          borderBottom: this.lineSize + " solid " + this.lineColor
+        }
       }
     },
     methods: {
-      clickItem: function (selectedItem, index) {
+      clickItem (selectedItem, index) {
         this.local_selectedIndex = index;
         this.$emit('selectChange', selectedItem, index);
+      },
+      updateHoverState (index, isHover) {
+        if (isHover) {
+          this.hoverItemIndex = index;
+        } else if (index === this.hoverItemIndex) { // 비동기 호출을 대비한 최소한의 if
+          this.hoverItemIndex = -1;
+        }
+      },
+      isHighlighted (index) { // 선택 됐거나 hover 됐거나 값
+        return index === this.local_selectedIndex || index === this.hoverItemIndex;
+      },
+      getLineStyle (index) {
+        return this.isHighlighted(index) ? this.underLine : this.blankLine;
       }
     }
   }
@@ -46,12 +75,5 @@
     float: left;
     margin-left: 0.8em;
     margin-right: 0.8em;
-    border-bottom: 0.3em solid #00000000;
-  }
-  .itemContainer:hover {
-    border-bottom: 0.3em solid white;
-  }
-  .selected {
-    border-bottom: 0.3em solid white;
   }
 </style>
---
+++ src/sample/problem/SelectableListSample.vue
@@ -0,0 +1,35 @@
+<template>
+  <selectable-list :items="items" lineColor="black" line-size="0.2em">
+    <div
+      slot="item"
+      slot-scope="props"
+    >
+      {{ props.element.text }}
+    </div>
+  </selectable-list>
+</template>
+
+<script>
+  import SelectableList from "@/components/problem/SelectableList";
+
+  export default {
+    components: {
+      SelectableList
+    },
+    data () {
+      return {
+        items: [
+          { text : "1번문제" },
+          { text : "2번문제" },
+          { text : "3번문제" },
+          { text : "4번문제" },
+          { text : "5번문제" }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
