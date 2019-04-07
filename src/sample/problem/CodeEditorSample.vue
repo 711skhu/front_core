8c885b2ecb21877e714cb3164650b4ec37dcbde0
shouwn <sjonghyon@gmail.com>
2019-04-07 10:01:31 +0900

    #1 코드 에디터 태그 작성 - props : theme(테마), mode(코드 종류), init-code(초기에 적혀 있을 코드) - theme 와 mode 가 변경되면 에디터 테마와 종류 변경 - code getter 와 setter 로 상위 컴포넌트에서 에디터 code 수정 가능

---
+++ src/sample/problem/CodeEditorSample.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sample">
+    <code-editor
+      ref="editor"
+      :theme="theme"
+      :mode="mode"
+      :init-code="initCode"
+      class="editor"
+    >
+    </code-editor>
+    <button @click="$emit('text', $refs.editor.getCode())">test</button>
+    <button @click="$refs.editor.setCode('test')">test2</button>
+  </div>
+</template>
+
+<script>
+  import CodeEditor from "@/components/problem/CodeEditor";
+
+  export default {
+    components: {
+      CodeEditor
+    },
+    data () {
+      return {
+        theme: "dracula",
+        mode: "javascript",
+        initCode: "let test = function () {\n\n}"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .sample {
+    width: 500px;
+    height: 500px;
+  }
+  .editor {
+    font-size: 14pt;
+  }
+</style>
