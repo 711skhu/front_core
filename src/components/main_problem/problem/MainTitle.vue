da8a5766c57bd5aee172ae00c9ed7350ef187657
tails5555 <tails5555@naver.com>
2019-04-14 23:55:47 +0900

    #2 Init Main Title Component 메인 문제 목록 구현 중 상단부 구현 완료. 날짜 데이터는 localeString() 으로 가정하였음.

---
+++ src/components/main_problem/problem/MainTitle.vue
@@ -0,0 +1,27 @@
+<template>
+    <sui-grid>
+        <sui-grid-column width="eight">
+            <h1 is="sui-header">
+                <sui-icon name="archive" />
+                <sui-header-content>{{ title }}</sui-header-content>
+            </h1>
+            <p>{{ context }}</p>
+        </sui-grid-column>
+        <sui-grid-column width="eight">
+            <h1 is="sui-header">
+                <sui-icon name="bell outline" />
+                <sui-header-content>제출기한</sui-header-content>
+            </h1>
+            <p>{{ limit_date }}</p>
+        </sui-grid-column>
+    </sui-grid>
+</template>
+
+<script>
+export default {
+    name: 'main-title',
+    props: [
+        'title', 'limit_date', 'context'
+    ],
+}
+</script>
---
+++ src/pages/main_problem/MainProblemListPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div is="sui-container">
+        <main-title 
+            :title="examination.title"
+            :limit_date="examination.limit_date"
+            :context="examination.context"
+        ></main-title>
+    </div>
+</template>
+
+<script>
+import MainTitle from '@/components/main_problem/problem/MainTitle'
+export default {
+    name: 'main-problem-list-page',
+    components: {
+        MainTitle
+    },
+    data() {
+        return {
+            examination: {
+                title: '1차 구현시험',
+                limit_date: new Date().toLocaleString(),
+                context: '실습, 과제, 시험에 대한 특이 사항을 기재합니다.',
+                problems: []
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>
