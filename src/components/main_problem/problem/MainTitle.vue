7bf760b2a901da52f3059a9bf9d59b225eba65d6
tails5555 <tails5555@naver.com>
2019-04-17 17:56:17 +0900

    #4 Update Main Title Component : Add Exam Type View 서버에서 받아온 시험 종류를 보여주는 것을 Vue 의 computed 를 사용하여 보여줌. 그리고 소단락(새끼) 문제들을 보여주는 UI 디자인 중.

--- src/components/main_problem/problem/MainTitle.vue
+++ src/components/main_problem/problem/MainTitle.vue
@@ -3,7 +3,10 @@
         <sui-grid-column width="eight">
             <h1 is="sui-header">
                 <sui-icon name="archive" />
-                <sui-header-content>{{ title }}</sui-header-content>
+                <sui-header-content>
+                    {{ title }}
+                    <sui-header-subheader>{{ unit_type }}</sui-header-subheader>
+                </sui-header-content>
             </h1>
             <p>{{ context }}</p>
         </sui-grid-column>
@@ -21,7 +24,22 @@
 export default {
     name: 'main-title',
     props: [
-        'title', 'limit_date', 'context'
+        'title', 'limit_date', 'context', 'type',
     ],
+    computed: {
+        unit_type() {
+            const { type } = this;
+            switch(type){
+                case 'EXAM' :
+                    return '구현 시험';
+                case 'HOMEWORK' :
+                    return '구현 과제';
+                case 'TRAIN' :
+                    return '구현 실습';
+                default :
+                    return '기타 항목';
+            }
+        }
+    }
 }
 </script>
--- src/pages/main_problem/MainProblemListPage.vue
+++ src/pages/main_problem/MainProblemListPage.vue
@@ -1,19 +1,23 @@
 <template>
     <div is="sui-container">
         <main-title 
+            :type="examination.type"
             :title="examination.title"
             :limit_date="examination.limit_date"
             :context="examination.context"
         ></main-title>
+        <main-element :problem="problem" :key="problem.id" v-for="problem in examination.problems"></main-element>
     </div>
 </template>
 
 <script>
 import MainTitle from '@/components/main_problem/problem/MainTitle'
+import MainElement from '@/components/main_problem/problem/MainElement'
+
 export default {
     name: 'main-problem-list-page',
     components: {
-        MainTitle
+        MainTitle, MainElement
     },
     data() {
         return {
@@ -21,7 +25,51 @@ export default {
                 title: '1차 구현시험',
                 limit_date: new Date().toLocaleString(),
                 context: '실습, 과제, 시험에 대한 특이 사항을 기재합니다.',
-                problems: []
+                type: 'TRAIN', // TRAIN 은 실습, HOMEWORK 는 과제, EXAM 는 구현 시험입니다.
+                problems: [ // problems 는 소단락 문제(일명 새끼 문제)들 목록입니다.
+                    {
+                        id: 1,
+                        title: 'JAVA ArrayList',
+                        upload_cnt: 10, // 이는 서버에서 제출한 사람의 수를 카운팅하여 반환하여 목록에서 보여줌을 가정하고 컴포넌트에 렌더링합니다.
+                        full_score: 10,
+                    },
+                    {
+                        id: 2,
+                        title: 'JAVA Arrays',
+                        upload_cnt: 12,
+                        full_score: 10,
+                    },
+                    {
+                        id: 3,
+                        title: 'JS Prototype',
+                        upload_cnt: 16,
+                        full_score: 20,
+                    },
+                    {
+                        id: 4,
+                        title: 'JS Object',
+                        upload_cnt: 19,
+                        full_score: 20,
+                    },
+                    {
+                        id: 5,
+                        title: 'JS Prototype',
+                        upload_cnt: 21,
+                        full_score: 20,
+                    },
+                    {
+                        id: 6,
+                        title: 'JS Object',
+                        upload_cnt: 13,
+                        full_score: 20,
+                    },
+                    {
+                        id: 7,
+                        title: '시험성적 확인전용 문제',
+                        upload_cnt: 18,
+                        full_score: 20,
+                    }
+                ]
             }
         }
     }
