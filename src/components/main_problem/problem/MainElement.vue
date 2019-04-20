bb1d9d8fc899c57ed41a9688982720e1db225f2d
tails5555 <tails5555@naver.com>
2019-04-20 23:24:31 +0900

    #4 Add Access Button 실습, 과제, 구현셤 별로 디테일한 접속 버튼 생성 완료. 시험 시간은 90분으로 잡아서 계산했음.

--- src/components/main_problem/problem/MainElement.vue
+++ src/components/main_problem/problem/MainElement.vue
@@ -2,12 +2,29 @@
     <sui-segment :color="top_color" clearing>
         <h2 is="sui-header" floated="left">
             {{ problem.title }}
-            <sui-header-subheader>
-                {{ problem.upload_cnt }} 명 제출 했습니다.
+            <sui-header-subheader v-if="new Date() >= problem.reserve_date">
+                <sui-icon name="users" /> 
+                <sui-header-content>
+                    {{ problem.upload_cnt }} 명 제출 했습니다.
+                </sui-header-content>
+            </sui-header-subheader>
+            <sui-header-subheader v-else>
+                <sui-icon name="clock outline" /> 
+                <sui-header-content>
+                    {{ problem.reserve_date.toLocaleString() }}
+                </sui-header-content>
             </sui-header-subheader>
         </h2>
-        <h2 is="sui-header" floated="right">
-            {{ my_score }} / {{ problem.full_score }}
+        <h2 is="sui-header" floated="right" text-align="center">
+            <div v-if="new Date() >= problem.reserve_date">
+                {{ my_score }} / {{ problem.full_score }}
+            </div>
+            <div v-else>
+                {{ problem.full_score }} pt
+            </div>
+            <sui-header-subheader>
+                <sui-button :content="btn_context.context" :color="btn_context.color" @click="btn_context.action" />
+            </sui-header-subheader>
         </h2>
     </sui-segment>
 </template>
@@ -15,7 +32,7 @@
 <script>
 export default {
     name: 'main-element',
-    props: ['problem', ],
+    props: ['problem', 'type', ],
     data() {
         return {
             my_score: 10, // 각 개인의 점수는 AJAX 를 사용해서 불러온다고 가정합니다.
@@ -24,11 +41,46 @@ export default {
     computed: {
         top_color() {
             const { my_score, problem } = this;
-            if(my_score === null) { // 현재 학생이 맞은 점수가 없으면 회색을 반환합니다.
+            const now = new Date();
+            if(my_score === null || now < problem.reserve_date) { // 현재 학생이 맞은 점수가 없으면 회색을 반환합니다.
                 return 'grey';
             }
 
             return my_score / problem.full_score > 0.5 ? 'green' : 'red'; // 점수가 절반을 넘기면 초록색, 안 넘기면 빨간색으로 나오게 합니다.
+        },
+        btn_context() {
+            const { problem, type } = this;
+            const now = new Date();
+            const dist = problem.reserve_date.getTime() - now.getTime();
+            switch(type){
+                case 'TRAIN' :
+                    return {
+                        color: 'grey',
+                        context: '실습하기',
+                        action: () => alert('실습 페이지')
+                    };
+                case 'HOMEWORK' :
+                    return {
+                        color: now < problem.reserve_date ? 'blue' : 'red',
+                        context: now < problem.reserve_date ? '제출하기' : '제출 기한 초과',
+                        action: now < problem.reserve_date ? () => alert('과제 제출 페이지') : () => alert('과제 제출을 못 합니다.')
+                    };
+                case 'EXAM' :
+                    return {
+                        color: 
+                            (now < problem.reserve_date) ?
+                                (dist < 90 * 60 * 1000) ? 'green' : 'yellow' // 시험 종료 90분 전에 접속 가능하게 합니다. 아니면 노란 버튼으로 접속 불가를 표기합니다.
+                                : 'blue', // 시험이 끝난 뒤에는 성적 공개 버튼으로 바꿉니다.
+                        context: 
+                            (now < problem.reserve_date) ?
+                                (dist < 90 * 60 * 1000) ? '시험 접속' : '접속 불가'
+                                : '점수 조회',
+                        action: 
+                            (now < problem.reserve_date) ?
+                                (dist < 90 * 60 * 1000) ? () => alert('시험 접속 페이지') : () => alert('시험은 90분 이전에 시작 됩니다!')
+                                : () => alert('점수 조회 Collapse!'),
+                    }
+            }
         }
     }
 }
--- src/pages/main_problem/MainProblemListPage.vue
+++ src/pages/main_problem/MainProblemListPage.vue
@@ -7,7 +7,12 @@
             :limit_date="examination.limit_date"
             :context="examination.context"
         ></main-title>
-        <main-element :problem="problem" :key="problem.id" v-for="problem in examination.problems"></main-element>
+        <main-element 
+            :key="problem.id"
+            :problem="problem" 
+            :type="examination.type" 
+            v-for="problem in examination.problems"
+        ></main-element>
     </div>
 </template>
 
@@ -27,56 +32,56 @@ export default {
                 title: '1차 구현시험',
                 limit_date: new Date().toLocaleString(),
                 context: '실습, 과제, 시험에 대한 특이 사항을 기재합니다.',
-                type: 'TRAIN', // TRAIN 은 실습, HOMEWORK 는 과제, EXAM 는 구현 시험입니다.
+                type: 'EXAM', // TRAIN 은 실습, HOMEWORK 는 과제, EXAM 는 구현 시험입니다.
                 problems: [ // problems 는 소단락 문제(일명 새끼 문제)들 목록입니다.
                     {
                         id: 1,
                         title: 'JAVA ArrayList',
                         upload_cnt: 10, // 이는 서버에서 제출한 사람의 수를 카운팅하여 반환하여 목록에서 보여줌을 가정하고 컴포넌트에 렌더링합니다.
                         full_score: 10,
-                        reserve_date: new Date(2019, 4, 17, 18, 0, 0),
+                        reserve_date: new Date(2019, 3, 17, 18, 0, 0),
                     },
                     {
                         id: 2,
                         title: 'JAVA Arrays',
                         upload_cnt: 12,
                         full_score: 10,
-                        reserve_date: new Date(2020, 4, 30, 18, 0, 0),
+                        reserve_date: new Date(2020, 3, 30, 18, 0, 0),
                     },
                     {
                         id: 3,
                         title: 'JS Prototype',
                         upload_cnt: 16,
                         full_score: 20,
-                        reserve_date: new Date(2019, 4, 17, 18, 0, 0),
+                        reserve_date: new Date(2019, 3, 17, 18, 0, 0),
                     },
                     {
                         id: 4,
                         title: 'JS Object',
                         upload_cnt: 19,
                         full_score: 20,
-                        reserve_date: new Date(2020, 4, 30, 18, 0, 0),
+                        reserve_date: new Date(2020, 3, 30, 18, 0, 0),
                     },
                     {
                         id: 5,
                         title: 'Python Dictionary',
                         upload_cnt: 21,
                         full_score: 20,
-                        reserve_date: new Date(2019, 4, 17, 18, 0, 0),
+                        reserve_date: new Date(2019, 3, 17, 18, 0, 0),
                     },
                     {
                         id: 6,
                         title: 'Python Lambda',
                         upload_cnt: 13,
                         full_score: 20,
-                        reserve_date: new Date(2019, 4, 17, 18, 0, 0),
+                        reserve_date: new Date(2019, 3, 21, 0, 0, 0),
                     },
                     {
                         id: 7,
                         title: '시험성적 확인전용 문제',
                         upload_cnt: 18,
                         full_score: 20,
-                        reserve_date: new Date(2020, 4, 30, 18, 0, 0),
+                        reserve_date: new Date(2020, 3, 30, 18, 0, 0),
                     }
                 ]
             }
