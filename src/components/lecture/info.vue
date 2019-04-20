8686f1838835d3c297c8db1e5b153e6d6a376b1e
jihyuno301 <joh755807@gmail.com>
2019-04-20 14:39:56 +0900

    강좌소개내용 마크다운 구현

--- src/components/lecture/info.vue
+++ src/components/lecture/info.vue
@@ -2,28 +2,37 @@
 
     <sui-container class="infobox" fluid >
       <!--강좌제목-->
-      <h1 is="sui-header" v-text="lectureTitle" class="title-style"></h1>
+      <h1 is="sui-header" v-text="lectureTitle" style="color:black"></h1>
       <!--강좌소개-->
       <div >
-        <!--md으로 소개내용 삽입예정-->
+        <!--강좌 소개내용-->
+        <markdownview v-bind:markdown="message"></markdownview>
         <!--강좌소개 옆 이미지-->
-        <i v-bind:class="selectedImage" style="color:white"></i>
+        <i v-bind:class="selectedImage" style="color:black"></i>
       </div>
     </sui-container>
 
 </template>
 
 <script>
+  import MarkdownView from '../MarkdownView.vue'
 
   export default {
     name : 'FluidExample',
+
     data () {
       return {
         lectureTitle : 'JAVA Programming',
         images : [ 'fas fa-balance-scale fa-10x', 'fas fa-book fa-10x', 'fas fa-atom fa-10x', 'fas fa-coins fa-10x', 'fas fa-dna fa-10x'],
-        selectedImage: ''
+        selectedImage: '',
+        message : '#### 자바언어 기초과정입니다'
       }
     },
+
+    components : {
+      'markdownview' : MarkdownView
+    },
+
     created() {
       const idx = Math.floor(Math.random() * this.images.length)
       this.selectedImage = this.images[idx]
@@ -38,16 +47,8 @@
   },
 
   .infobox {
-    background-color : grey;
     padding-right : 60px;
     padding-left : 100px;
   }
-
-  .title-style {
-    color : white;
-  }
-
-
-
 
 </style>
