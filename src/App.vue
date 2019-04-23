eabcd0a89f2bf7ff6b998a5f4d9ecf3a77279392
jeenie <jeenie828@gmail.com>
2019-04-23 22:09:51 +0900

    #6 강의 목록을 위한 LectureItem 구현

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app">
+    <lecture-item></lecture-item>
   </div>
 </template>
+
+<script>
+  import LectureItem from "@/components/lecture/LectureItem";
+
+  export default {
+    components: {
+      LectureItem
+    }
+  }
+</script>
 
 <style lang="less">
 #app {
   margin-top: 1.5rem;
   line-height: 1.4285;
+  height: 100%;
 }
 </style>
---
+++ src/components/lecture/LectureItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ui middle aligned divided list">
+    <div class="item margin-left-30">
+      <a>
+        <div class="content">
+          <div class="header">알고리즘</div>
+          <h5>이승진</h5>
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="public">
+            <label>활성화</label>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="item margin-left-30">
+      <a>
+        <div class="content">
+          <div class="header">운영체제</div>
+          <h5>유상신</h5>
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="public">
+            <label>활성화</label>
+          </div>
+        </div>
+      </a>
+    </div><div class="item margin-left-30">
+      <a>
+        <div class="content">
+          <div class="header">데이테베이스개론</div>
+          <h5>홍은지</h5>
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="public">
+            <label>활성화</label>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .margin-left-30 {
+    margin-left: 30px
+  }
+</style>
