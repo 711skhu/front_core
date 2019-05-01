1304f07989c86996273fc4a895c61efe98a148ee
shouwn <sjonghyon@gmail.com>
2019-05-02 01:00:56 +0900

    코드 에디터 수정 및 테마 선택 버튼, 언어 선택 드롭 다운 구현

---
+++ src/components/problem/GroupButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <sui-button-group>
+    <sui-button basic
+      v-for="option in options"
+      :key="option.toString()"
+      :active="isActive(option)"
+      @click="onClick(option)">
+      {{ option.toString() }}
+    </sui-button>
+  </sui-button-group>
+</template>
+
+<script>
+  export default {
+    model: {
+      prop: "active",
+      event: "change"
+    },
+    props: {
+      options: {
+        type: Array,
+        required: true
+      },
+      active: {
+        required: false
+      }
+    },
+    methods: {
+      isActive(option) {
+        return this.active === option;
+      },
+      onClick(option) {
+        this.$emit('change', option);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
