c3d40e57fc48ea264ca0d76b8a36ef24ca2db211
jeenie <jeenie828@gmail.com>
2019-05-08 21:31:50 +0900

    #9 LectureInfo 컴포넌트 분리

---
+++ src/components/lecture/LectureInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="content">
+    {{ props.element.title }}
+    <div class="sub header"> {{ props.element.professor }} </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
