63a8697bb6d58c64f2288d8a1e54aa65a18a13a2
jihyuno301 <joh755807@gmail.com>
2019-05-13 01:52:15 +0900

    #8 문제type 샘플구현

---
+++ src/sample/lecture_type/info.vue
@@ -0,0 +1,55 @@
+<template>
+
+    <sui-container class="infobox" fluid >
+      <!--강좌제목-->
+      <h1 is="sui-header" v-text="lectureTitle" style="color:black"></h1>
+      <!--강좌소개-->
+      <div >
+        <!--강좌 소개내용-->
+        <markdownview v-bind:markdown="message"></markdownview>
+        <!--강좌소개 옆 이미지-->
+        <i v-bind:class="selectedImage" style="color:black"></i>
+      </div>
+    </sui-container>
+
+</template>
+
+<script>
+  import MarkdownView from '../MarkdownView.vue'
+
+  export default {
+    name : 'FluidExample',
+
+    data () {
+      return {
+        lectureTitle : 'JAVA Programming',
+        images : [ 'fas fa-balance-scale fa-10x', 'fas fa-book fa-10x', 'fas fa-atom fa-10x', 'fas fa-coins fa-10x', 'fas fa-dna fa-10x'],
+        selectedImage: '',
+        message : '#### 자바언어 기초과정입니다'
+      }
+    },
+
+    components : {
+      'markdownview' : MarkdownView
+    },
+
+    created() {
+      const idx = Math.floor(Math.random() * this.images.length)
+      this.selectedImage = this.images[idx]
+    }
+  };
+</script>
+
+<style lang="css" scoped>
+  i {
+    float : right;
+
+  },
+
+  .infobox {
+    padding-right : 60px;
+    padding-left : 100px;
+    padding-top : 20px;
+  }
+
+</style>
---
+++ src/sample/lecture_type/problem__type.vue
@@ -0,0 +1,57 @@
+<template>
+  <sui-segment>
+    <sui-header size="medium" textAlign="left">
+      {{ typeTitle }}
+      <sui-icon name="arrow right" size="big" class="title__icon"  />
+    </sui-header>
+
+    <sui-grid :columns="3">
+      <sui-grid-row>
+        <sui-grid-column>
+           <docs-wireframe name="paragraph" />
+           <h4 is="sui-header"> 맞은문제 </h4>
+           <p> {{ typeCorrectProblemCnt }} / {{ typeTotalProblemCnt }} </p>
+        </sui-grid-column>
+        <sui-grid-column>
+           <docs-wireframe name="paragraph" />
+           <h4 is="sui-header"> 틀린문제 </h4>
+           <p> {{ typeInCorrectProblemCnt }} / {{ typeTotalProblemCnt }} </p>
+        </sui-grid-column>
+        <sui-grid-column>
+           <docs-wireframe name="paragraph" />
+           <h4 is="sui-header"> 덜푼문제 </h4>
+           <p> {{ not_solve }} / {{ typeTotalProblemCnt }}</p>
+        </sui-grid-column>
+      </sui-grid-row>
+    </sui-grid>
+  </sui-segment>
+</template>
+
+<script>
+export default {
+  name : 'problem__type',
+  data() {
+    return {
+      typeId : 1,
+      typeTitle : '2019학년도 구현과제',
+      typeTotalProblemCnt : 10,
+      typeCorrectProblemCnt : 6,
+      typeInCorrectProblemCnt : 2
+    }
+  },
+  computed: {
+    not_solve() {
+      const { typeTotalProblemCnt, typeCorrectProblemCnt, typeInCorrectProblemCnt } = this;
+      return typeTotalProblemCnt-(typeCorrectProblemCnt+typeInCorrectProblemCnt);
+    }
+  }
+
+
+}
+</script>
+
+<style lang="css" scoped>
+.title__icon {
+  float : right;
+}
+</style>
