4016a2efb08dfa2e5c3b194579843621e9804664
jihyuno301 <joh755807@gmail.com>
2019-05-13 01:50:17 +0900

    #8 강의소개, 문제type 구현

---
+++ src/components/lecture_type/problem__type.vue
@@ -0,0 +1,71 @@
+<template>
+  <sui-segment>
+    <sui-header size="medium" textAlign="left">
+      {{ typeTitle }}
+      <sui-icon name="arrow right" size="big" class="title__icon"  />
+    </sui-header>
+    <sui-grid :columns="3">
+      <sui-grid-row>
+        <sui-grid-column>
+           <docs-wireframe name="paragraph" />
+           <h4 is="sui-header"> 맞은문제 </h4>
+           <p> {{ typeCorrectProblemCnt }} / {{ typeTotalProblemCnt }} </p>
+        </sui-grid-column>
+        <sui-grid-column>
+           <docs-wireframe name="paragraph" />
+           <h4 is="sui-header"> 틀린문제 </h4>
+           <p> {{ typeIncorrectProblemCnt }} / {{ typeTotalProblemCnt }} </p>
+        </sui-grid-column>
+        <sui-grid-column>
+           <docs-wireframe name="paragraph" />
+           <h4 is="sui-header"> 덜푼문제 </h4>
+           <p> {{ not_solve }} / {{ typeTotalProblemCnt }}</p>
+        </sui-grid-column>
+      </sui-grid-row>
+    </sui-grid>
+  </sui-segment>
+</template>
+
+<script>
+export default {
+  name : 'problem__type',
+  props : {
+    typeId : {
+      type : Number,
+      default : -1,
+      required : true
+    },
+    typeTitle : {
+      type : String,
+      required : true
+    },
+    typeTotalProblemCnt : {
+      type : Number,
+      required : true
+    },
+    typeCorrectProblemCnt : {
+      type : Number,
+      required : true
+    },
+    typeIncorrectProblemCnt : {
+      type : Number,
+      required : true
+    }
+  },
+  computed: {
+    not_solve() {
+      const { typeTotalProblemCnt, typeCorrectProblemCnt, typeIncorrectProblemCnt } = this;
+      return typeTotalProblemCnt-(typeCorrectProblemCnt+typeIncorrectProblemCnt);
+    }
+  }
+
+
+}
+</script>
+
+<style lang="css" scoped>
+.title__icon {
+  float : right;
+}
+
+</style>
