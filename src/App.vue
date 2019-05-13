56b54a93dbbe559f32803d767a5e073c2b0ca527
jeenie <jeenie828@gmail.com>
2019-05-13 23:27:12 +0900

    App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <template>
   <div id="app">
-    <header-manu></header-manu>
   </div>
 </template>
-
-<script>
-  import HeaderManu from "@/components/header/HeaderManu";
-
-  export default {
-    components: {
-      HeaderManu
-    }
-  }
-</script>
 
 <style lang="less">
 #app {
   margin-top: 1.5rem;
   line-height: 1.4285;
-  height: 100%;
 }
 </style>
