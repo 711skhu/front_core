26d71fcfed3f23b80ba739ee1856ba1595d83a23
jeenie <jeenie828@gmail.com>
2019-05-13 23:23:49 +0900

    #11 헤더 메뉴바 재구성

--- src/components/header/HeaderManu.vue
+++ src/components/header/HeaderManu.vue
@@ -2,12 +2,13 @@
   <div class="ui inverted segment">
     <div class="ui inverted secondary pointing menu">
       <div class="item">SKHU Online Judge</div>
-      <div>
-        <a class="item">스킬 체크</a>
-        <a class="item">개발자 채용</a>
-        <a class="item">코딩테스트 연습</a>
-        <a class="item">프로그래밍 강의</a>
+      <div class="margin-right-20p"></div>
+      <div class="ui inverted secondary pointing menu">
+        <a v-for="(menu, index) in menus" class="item" v-bind:class="{ active: getActive }" v-bind:href="menu.url" @click="setActive(menu.id)">
+          {{ menu.title }}
+        </a>
       </div>
+      <div class="margin-left-20p"></div>
       <div class="right menu">
         <a class="item">로그인</a>
         <a class="item">회원가입</a>
@@ -15,3 +16,56 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      menus: [
+        {
+          id: 1,
+          title: '스킬체크',
+          url: '/skill'
+        },
+        {
+          id: 2,
+          title: '개발채용',
+          url: '/employ'
+        },
+        {
+          id: 3,
+          title: '코딩테스 연습',
+          url: '/test'
+        },
+        {
+          id: 4,
+          title: '프로그래밍 강의',
+          url: '/lecture'
+        }
+      ],
+      isActive: -1
+    }
+  },
+  methods: {
+    setActive(index) {
+      this.isActive == index;
+    }
+  }
+  /*
+  computed: {
+    getActive: function () {
+      return this.isActive == this.menus.id
+    }
+  }
+  */
+}
+</script>
+
+<style>
+div.margin-right-20p {
+  margin-right: 20%
+}
+div.margin-left-20p {
+  margin-left: 20%
+}
+</style>
