450b7ff5bc68c22fcac7669ecb198c4304f62a05
jeenie <jeenie828@gmail.com>
2019-05-19 22:44:21 +0900

    #11 메인 헤더 메뉴를 위한 HeaderInfo 구현

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
+    <header-menu></header-menu>
   </div>
 </template>
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 =======
 <script>
@@ -16,6 +18,17 @@ export default {
 </script>
 
 >>>>>>> 51746ab704bc9ae609641329a65cca34bf10a8b1
+=======
+<script>
+import HeaderMenu from '@/components/header/HeaderManu'
+
+export default {
+  components: {
+    HeaderMenu
+  }
+}
+</script>
+>>>>>>> Stashed changes
 <style lang="less">
 #app {
   margin-top: 1.5rem;
--- src/components/header/HeaderManu.vue
+++ src/components/header/HeaderManu.vue
@@ -1,71 +1,46 @@
 <template>
-  <div class="ui inverted segment">
-    <div class="ui inverted secondary pointing menu">
-      <div class="item">SKHU Online Judge</div>
-      <div class="margin-right-20p"></div>
-      <div class="ui inverted secondary pointing menu">
-        <a v-for="(menu, index) in menus" class="item" v-bind:class="{ active: getActive }" v-bind:href="menu.url" @click="setActive(menu.id)">
-          {{ menu.title }}
-        </a>
+  <div>
+
+      <div>SKHU Online Judge</div>
+
+      <div>
+        <header-item
+          v-for="menu in menus"
+          v-model="currentMenu"
+          :menu="menu"
+        />
       </div>
-      <div class="margin-left-20p"></div>
-      <div class="right menu">
-        <a class="item">로그인</a>
-        <a class="item">회원가입</a>
+      
+      <div>
+        <a>로그인</a>
+        <a>회원가입</a>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
+import HeaderItem from "@/components/header/HeaderItem";
+
+const menus = [
+  new HeaderInfo("스킬체크", "/skill"),
+  new HeaderInfo("개발채용", "/employ"),
+  new HeaderInfo("코딩테스트 연습", "/test"),
+  new HeaderInfo("프로그래밍 강의", "/lecture")
+];
+
 export default {
+  components: {
+    HeaderItem
+  },
   data () {
     return {
-      menus: [
-        {
-          id: 1,
-          title: '스킬체크',
-          url: '/skill'
-        },
-        {
-          id: 2,
-          title: '개발채용',
-          url: '/employ'
-        },
-        {
-          id: 3,
-          title: '코딩테스 연습',
-          url: '/test'
-        },
-        {
-          id: 4,
-          title: '프로그래밍 강의',
-          url: '/lecture'
-        }
-      ],
-      isActive: -1
-    }
-  },
-  methods: {
-    setActive(index) {
-      this.isActive == index;
+      menus: menus,
+      currentMenu: null
     }
   }
-  /*
-  computed: {
-    getActive: function () {
-      return this.isActive == this.menus.id
-    }
-  }
-  */
 }
 </script>
 
-<style>
-div.margin-right-20p {
-  margin-right: 20%
-}
-div.margin-left-20p {
-  margin-left: 20%
-}
+<style scoped>
+
 </style>
