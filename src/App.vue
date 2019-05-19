6a0f85453da1b0a20695fd14ea7f4406b08d2e1b
jihyuno301 <joh755807@gmail.com>
2019-05-19 18:41:11 +0900

    #8 강의소개 for User

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
   <div id="app">
-    <Info></Info>
+
   </div>
 </template>
 <script>
-import Info from './sample/lecture_type/info.vue'
-export default {
-  components : {
-    'Info' : Info
-  }
-}
+
 </script>
 
 
@@ -17,7 +12,6 @@ export default {
 #app {
   margin-top: 1.5rem;
   line-height: 1.4285;
-  width : 60%;
 }
 
 </style>
