289512c24a826f4b04a1969bffc8b38ab6d35ce5
shouwn <sjonghyon@gmail.com>
2019-05-23 00:10:46 +0900

    문제 리스트 페이지 컴포넌트 작성

--- src/components/lecture/LectureItem.vue
+++ src/components/lecture/LectureItem.vue
@@ -1,32 +1,30 @@
 <template>
-      <a>
-          <h3 class="ui header padding-24">
-            <lecture-info v-bind:lecture="lecture"></lecture-info>
-            <lecture-toggle></lecture-toggle>
-          </h3>
-      </a>
+  <a>
+    <h3 class="ui header padding-24">
+      <lecture-info v-bind:lecture="lecture"></lecture-info>
+      <lecture-toggle></lecture-toggle>
+    </h3>
+  </a>
 </template>
 
 
-
-
 <script>
-import LectureInfo from "@/components/lecture/LectureInfo"
-import LectureToggle from "@/components/lecture/LectureToggle"
+  import LectureInfo from "@/components/lecture/LectureInfo"
+  import LectureToggle from "@/components/lecture/LectureToggle"
 
-export default {
-  components: {
-    LectureInfo,
-    LectureToggle
-  },
-  props: ['lecture']
+  export default {
+    components: {
+      LectureInfo,
+      LectureToggle
+    },
+    props: ['lecture']
   }
 
 </script>
 
 <style scoped>
-.padding-24 {
-  padding: 24px;
-}
+  .padding-24 {
+    padding: 24px;
+  }
 
 </style>
---
+++ src/components/problem/ScoreList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="scores"
+      class="elevation-1"
+    >
+      <template v-slot:items="props">
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.score }}</td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+  import StudentScore from "@/models/problem/StudentScore";
+
+  let scores = [
+    new StudentScore("서종현", 10),
+    new StudentScore("서종현1", 100),
+    new StudentScore("서종현3", 60),
+    new StudentScore("서종현5", 20),
+    new StudentScore("서종현2", 50),
+    new StudentScore("서종현4", 90),
+  ];
+
+  export default {
+    props: {
+      maxScore: {
+        type: Number,
+        required: true,
+        description: "만점 성적"
+      }
+    },
+    data() {
+      return {
+        headers: [
+          {text: '이름', value: 'name'},
+          {text: `점수 (만점: ${this.maxScore})`, value: 'score'},
+        ],
+        scores: scores
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
