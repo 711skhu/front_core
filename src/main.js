8f9b699df2cf68ba5f4b5092650d4dd9a3cc3158
shouwn <sjonghyon@gmail.com>
2019-05-22 20:29:04 +0900

    Vuetify 적용

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 
 import 'semantic-ui-less/semantic.less'
 import SuiVue from 'semantic-ui-vue';
 
+import 'vuetify/dist/vuetify.min.css'
+
 Vue.use(SuiVue);
+Vue.use(Vuetify);
 
 new Vue({
   el: '#app',
