bdce348c19a47324f7487f0d6b86d67442ccc2cc
jeenie <jeenie828@gmail.com>
2019-06-09 22:45:25 +0900

    App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <v-app id="app">
-    <lecture-list-page></lecture-list-page>
+
   </v-app>
 </template>
 <script>
- import LectureListPage from '@/admin/views/lectureList/LectureListPage'
- //import LectureAdd from '@/admin/views/lectureList/LectureAdd'
 
-  export default {
-   components: {
-     //LectureAdd
-     LectureListPage
-   }
-  }
 </script>
 <style lang="less">
   #app {
