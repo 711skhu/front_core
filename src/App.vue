a1d010908d847dd8db8f7c80b32c71a74d077388
jeenie <jeenie828@gmail.com>
2019-06-22 20:49:13 +0900

    App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <v-app id="app"></v-app>
+  <v-app id="app">
+    <login-page></login-page>
+  </v-app>
 </template>
+<script>
+  import LoginPage from '@/pc/views/login/Login'
 
+  export default {
+    components: {
+      LoginPage
+    }
+  }
+</script>
 <style lang="less">
   #app {
     line-height: 1.4285;
