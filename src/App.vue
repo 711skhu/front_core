c8ecff63004f1fba9a2d1575cfe6c24be110d065
jeenie <jeenie828@gmail.com>
2019-06-22 21:45:34 +0900

    App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <v-app id="app">
-
+    <sign-up-page></sign-up-page>
   </v-app>
 </template>
 <script>
+  import SignUpPage from '@/admin/views/signup/SignUpForm'
 
+  export default {
+    components: {
+      SignUpPage
+    }
+  }
 </script>
 <style lang="less">
   #app {
